8af44ef4b76f849fd4cccb4f499d0d88e50a4577
Ali Faraji <ali@01d.ir>
2021-11-21 23:42:33 +0330

    access point mode and static IP for AP in NodeMCU ESP8266

---
+++ access_point_mode/access_point_mode.ino
@@ -0,0 +1,35 @@
+#include <ESP8266WiFi.h>
+
+char ssid[] = "Network-1";          //  your network SSID (name) 
+
+IPAddress IP = {10, 10, 1, 1};
+IPAddress gateway = {10, 10, 1, 1};
+IPAddress NMask = {255, 255, 255, 0};
+
+WiFiServer server(80);
+
+void setup ()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("System Start");
+
+  WiFi.mode(WIFI_AP);  
+  WiFi.softAP(ssid);
+  delay(1000);
+  
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP()); 
+  
+  WiFi.softAPConfig(IP, IP, NMask);
+
+  delay(1000);
+  
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());  
+
+  server.begin();
+}
+
+void loop ()
+{ }
